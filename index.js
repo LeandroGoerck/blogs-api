b6b642c9428b37e3efd986d3a3aa6082f234b8cb
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-04-10 22:45:11 -0300

    created add users migration

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-// startingsdsads
+require('dotenv').config();
+
 const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
-  response.send();
+  response.send('Franciela meu Amor!');
 });
