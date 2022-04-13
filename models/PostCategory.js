0fbc6189e52bd21d55cb90227e331bd81483e607
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-04-12 21:06:56 -0300

    lint fixes lint issues on PostCategory models

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -2,22 +2,14 @@ module.exports = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define('PostCategory', {
     postId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     categoryId: DataTypes.STRING,
-  }, {
-    timestamps: false,
-    tableName: 'PostCategories',
-  });
+  }, { timestamps: false, tableName: 'PostCategories' });
   
   PostCategory.associate = (models) => {
     models.BlogPost.belongsToMany(models.Category, {
-      as: 'categories', through: PostCategory,
-      foreignKey: 'categoryId',
-      otherKey: 'postId',
+      as: 'categories', through: PostCategory, foreignKey: 'categoryId', otherKey: 'postId',
     });
     models.Category.belongsToMany(models.BlogPost, {
-      as: 'posts',
-      through: PostCategory,
-      foreignKey: 'postId',
-      otherKey: 'categoryId',
+      as: 'posts', through: PostCategory, foreignKey: 'postId', otherKey: 'categoryId',
     });
   };
 
